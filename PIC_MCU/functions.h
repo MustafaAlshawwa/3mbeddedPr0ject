b9ba034cbf83e91a15e952eaef1ca6951907b209
Lonely_Mandoz <65305035+OmarAL-Qtaishat@users.noreply.github.com>
2022-12-26 13:49:08 +0300

    Update functions.h

--- PIC_MCU/functions.h
+++ PIC_MCU/functions.h
@@ -24,6 +24,27 @@ void Buzzer_off()
  PORTC = PORTC & 0xFE; // Here all pins are same except for RC0 is off
 }
 
+# A function to control the direction of the motors 
+void move_forward(){
+ PORTD = 0x05; // Here all pins are same except for RC0 is on 
+}
+
+# A function to control the direction of the motors 
+void move_back(){
+ PORTD = 0x0A; // Here all pins are same except for RC0 is off 
+}
+
+# A function to control the direction of the motors
+void move_right(){
+  PORTD = 0x09; // Here all pins are zero except for RC0 and RC3
+}
+
+# A function to control the direction of the motors
+void move_left(){
+  PORTD = 0x06; // Here all pins are zero except for RC1 and RC2 
+}
+
+
 # Demo function for interrupt vector at RB0
 void interrupt()
 {
