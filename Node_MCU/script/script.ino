d5778543656a34402efbfab8b2fca54a49356159
NaturalT314 <74961214+NaturalT314@users.noreply.github.com>
2023-01-23 17:17:36 +0300

    Update script.ino

--- Node_MCU/script/script.ino
+++ Node_MCU/script/script.ino
@@ -1,23 +1,29 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
-//  WiFi credentials
 const char* ssid = "OrangeFlybox_1FD4";
 const char* password = "70323320";
+WebServer server(80); 
 
-// movement signal pins
-const int RC5 = D5;
-const int RC6 = D6;
-const int RC7 = D7;
+const int RC5 = 13;
+const int RC6 = 12;
+const int RC7 = 14;
 
-ESP8266WebServer server(80);
-
-LiquidCrystal_I2C lcd(0x27,16,2);
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
 
 void handleStop() {
-  digitalWrite(RC5, 0);
+  digitalWrite(RC5, LOW);
+  digitalWrite(RC6, LOW);
+  digitalWrite(RC7, LOW);
 }
 
 void handleForward() {  
@@ -44,50 +50,23 @@ void handleLeft() {
   digitalWrite(RC7, LOW);
 }
 
-void handleLCD() {
-  lcd.setCursor(0,0);
-  lcd.clear();
-  lcd.print(server.arg(0));
-  Serial.print(server.arg(0) + "\r\n");
-}
-
 void setup() {
-//  Init Signal pins as output
+  Serial.begin(9600);
+  
   pinMode(RC5, OUTPUT);
   pinMode(RC6, OUTPUT);
   pinMode(RC7, OUTPUT);
 
-//  Init LCD  
-  lcd.init(); 
-  lcd.backlight();
-  
+  initWiFi();
 
-//  Init WiFi
-  WiFi.begin(ssid, password);
-  Serial.begin(9600);
 
-//  Print on serial until Connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-//  Init server commands
   server.on("/forward", handleForward);
   server.on("/reverse", handleReverse);
   server.on("/left", handleLeft);
   server.on("/right", handleRight);
   server.on("/stop", handleStop);
-  server.on("/lcd", handleLCD);
 
-// Begin Server
   server.begin();
-  Serial.println("Web server started\r\n");
-  Serial.println(WiFi.localIP());
-  lcd.clear();
-  lcd.print(WiFi.localIP());
 }
 
 void loop() {
